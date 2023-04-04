bc876c60fa4601dbc422b4279918abeb2033a925
fanpeichao <fan552426811@outlook.com>
2023-04-04 17:30:46 +0800

    spring

--- practice-java/src/main/java/org/example/MyIocContainer.java
+++ practice-java/src/main/java/org/example/MyIocContainer.java
@@ -13,15 +13,20 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MyIocContainer {
-    public static void main(String[] args) {
+    public static Map<String, Object> initBeans;
+
+    public static void init() {
         Properties properties = new Properties();
         try {
+            // 加载Bean配置文件（类映射文件）
             properties.load(MyIocContainer.class.getResourceAsStream("/ioc.properties"));
             System.out.println(properties);
             Map<String, Object> beans = new HashMap<>();
-            properties.forEach((beanName, beanClass) -> {
+            // properties：{orderDao=org.example.OrderDao, orderService=org.example.OrderService}
+            // 实例化配置文件中所有的 Bean 并放到Map集合 beans 中
+            properties.forEach((beanName, fullyQualifiedClassName) -> {
                 try {
-                    Class klass = Class.forName((String) beanClass);
+                    Class klass = Class.forName((String) fullyQualifiedClassName);
                     Object beanInstance = klass.getConstructor().newInstance();
                     beans.put((String) beanName, beanInstance);
                 } catch (ClassNotFoundException | InvocationTargetException | InstantiationException |
@@ -29,31 +34,39 @@ public class MyIocContainer {
                     throw new RuntimeException(e);
                 }
             });
+            // 遍历beans中的实例化对象，使用反射获取对应实例的依赖
             beans.forEach((beanName, beanInstance) -> {
-                dependencyinject(beanName, beanInstance, beans);
+                dependencyInject(beanName, beanInstance, beans);
             });
-            OrderService orderService = (OrderService) beans.get("orderService");
-            OrderDao orderDao = (OrderDao) beans.get("orderDao");
-
+            initBeans = beans;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void dependencyinject(String beanName, Object beanInstance, Map<String, Object> beans) {
-        List<Field> fieldsTobeAutowired = Stream.of(beanInstance.getClass()
-                        .getDeclaredFields())
+    private static void dependencyInject(String beanName, Object beanInstance, Map<String, Object> beans) {
+        // 获取有Autowired注解的字段
+        List<Field> fieldsTobeAutowired = Stream.of(beanInstance.getClass().getDeclaredFields())
                 .filter(field -> field.getAnnotation(Autowired.class) != null)
                 .collect(Collectors.toList());
         fieldsTobeAutowired.forEach(field -> {
             try {
                 String fieldName = field.getName();
+                // 在已经实例化所有所需要的bean对象的beans中查找依赖的那个实例对象
                 Object depencyBeanInstance = beans.get(fieldName);
                 field.setAccessible(true);
+                // 将当前对象所依赖的对象注入
                 field.set(beanInstance, depencyBeanInstance);
             } catch (IllegalAccessException e) {
                 throw new RuntimeException(e);
             }
         });
+    }
+
+    public static void main(String[] args) {
+        // 执行
+        init();
+        OrderService orderService = (OrderService) initBeans.get("orderService");
+        OrderDao orderDao = (OrderDao) initBeans.get("orderDao");
     }
 }
